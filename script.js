d2e5f58ad2421f77b2f73b9cc201d821b51a0aee
António Kudiezo <kudiezo527@gmail.com>
2024-08-05 17:59:14 +0100

    Add script

---
+++ script.js
@@ -0,0 +1,8 @@
+const handlerBtnFollowOver = () => {
+    const btn_follow = document.getElementById("btn-follow")
+    btn_follow.innerHTML = "Follow me &#x25B2;"
+}
+const handlerBtnFollowOut = () => {
+    const btn_follow = document.getElementById("btn-follow")
+    btn_follow.innerHTML = "Follow me &#x25BC"
+}
